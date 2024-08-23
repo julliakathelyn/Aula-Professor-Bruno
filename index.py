a9b6e09c883a4aff9aa67ae8284a786f67f719e3
Jullia <137806375+julliakathelyn@users.noreply.github.com>
2024-08-22 22:08:35 -0300

    aula 1

---
+++ index.py
@@ -0,0 +1,19 @@
+print ("Digite o primeiro numero")
+x = (int) (input());
+print("Digite o segundo numero")
+y = (int)(input())
+
+z = x+y
+print (z)
+
+if (z > 10):
+   print("maior")
+else:
+   print("menos")
+
+if (x == 10):
+   print("igual")
+elif(x < 10):
+   print("Menor")
+else :
+   print("maior")
