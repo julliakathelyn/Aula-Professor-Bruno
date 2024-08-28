4a780a3502e6b8bbd4d499d6ca6002be1a679f86
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-08-28 11:57:04 -0300

    Add files via upload

---
+++ Aula 2/exercicio1.py
@@ -0,0 +1,21 @@
+#cire uma função que some 2 valores
+def soma (x,y):
+  z = x + y
+  return z
+
+def main():
+  a = (float)(input("Digite x"))
+  b = (float)(input("Digite y"))
+  print(soma(a,b))
+  
+#main()
+
+a = [1, 2, 3]
+b = [9, 8, 7]
+
+print(soma(a,b))
+
+a = "Jullia "
+b = "Kathelyn"
+
+print(soma(a,b))
